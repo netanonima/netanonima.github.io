bd6d44bfb8b57b726f99bc2a083576c3f21279bd
Flavio Bertolini <info@noobill.ch>
2023-08-23 20:41:40 +0200

    try to solve deployment issue

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,5 @@
 </template>
 
 <style scoped>
-.navbar-selected-link{
-  @apply block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500
-}
-.navbar-link{
-  @apply block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent
-}
-.view{
-  @apply px-40 pt-5
-}
-footer{
-  @apply mt-10
-}
+
 </style>
